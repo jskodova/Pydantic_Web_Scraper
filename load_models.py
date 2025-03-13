e0f0e47a2d48668022c52c0150e1efc7d8f4e178
Jarmila Škodová <89833620+jskodova@users.noreply.github.com>
2025-03-13 22:14:02 +0200

    Changed to models finetuned for tool use

--- load_models.py
+++ load_models.py
@@ -1,8 +1,6 @@
 import os
 from pydantic_ai.models.openai import OpenAIModel
 
-
-
 OPENAI_MODEL = OpenAIModel(model_name='gpt-4o-2024-11-20')
 
-OLLAMA_MODEL = OpenAIModel(model_name='llama3.1', base_url='http://localhost:11434/v1' )
+OLLAMA_MODEL = OpenAIModel(model_name='llama3-groq-tool-use', base_url='http://localhost:11434/v1')
